bc067b122a1aac679d3b4d22ad656d96920e4275
Francis Ong <francis.ong.pixel8@gmail.com>
2022-04-20 09:41:16 +0800

    add contact page content and style

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,6 +1,41 @@
+import './../css/ContactPage.scss';
+
 const Contact = () => (
-  <div className="contact-container">
-    <h1>Contact Page</h1>
+  <div className="contact-container p-5 m-5">
+    <form method="post">
+      <div className='row'>
+        <div className='col-6 bg-contact'></div>
+        <div className='col-6 p-5 bg-light'>
+          <h1 className='mb-5 text-center'>Send Us a Message</h1>
+          <div className='form-group my-4'>
+            <label className="form-control text-white bg-primary">ENTER YOUR FULL NAME</label>
+            <div className='row'>
+              <div className='col-6 pe-0'>
+                <input type='text' placeholder='First name' className="p-3 form-control" />
+              </div>
+              <div className='col-6 ps-0'>
+                <input type='text' placeholder='Last name' className="p-3 form-control" />
+              </div>
+            </div>
+          </div>
+          <div className='form-group mb-4'>
+            <label className="form-control text-white bg-primary">ENTER YOUR EMAIL</label>
+            <input type='email' placeholder='E.g francis.ong25@gmail.com' className="col-6 p-3 mb-2 form-control" />
+          </div>
+          <div className='form-group mb-4'>
+            <label className="form-control text-white bg-primary">ENTER YOUR PHONE NUMBER</label>
+            <input type='text' placeholder='E.g 09000000000' className="col-6 p-3 mb-2 form-control" />
+          </div>
+          <div className='form-group mb-4'>
+            <label className="form-control text-white bg-primary">ENTER MESSAGE</label>
+            <textarea className='form-control' rows='7'></textarea>
+          </div>
+          <h5 className='text-center mt-3'>
+            <button className='btn btn-primary mt-3'>Submit</button>
+          </h5>
+        </div>
+      </div>
+    </form>
   </div>
 )
 
--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -11,24 +11,24 @@ const Events = () => (
       </div>
       <div className="carousel-inner rounded-5 shadow-4-strong">
         <div className="carousel-item active">
-          <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp" className="d-block w-100"
-            alt="Sunset Over the City" />
+          <img src="https://www.itinfrastructuresummit.com/assets/summit2022/img/og.jpeg" className="d-block w-100"
+            alt="Sunset Over the City" height='600' />
           <div className="carousel-caption d-none d-md-block">
             <h5>SyraTech Online Summit</h5>
             <p>SyraTech first ever Online event coming soon!</p>
           </div>
         </div>
         <div className="carousel-item">
-          <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" className="d-block w-100"
-            alt="Canyon at Nigh" />
+          <img src="https://www.rasmussen.edu/-/media/images/blogs/402_sotwe_9242015.jpg?la=en&hash=7FF9AA8F39093AB72DEA67CC5815829F25851CC7" className="d-block w-100"
+            alt="Canyon at Nigh" height='600' />
           <div className="carousel-caption d-none d-md-block">
             <h5>SyraTech Hackathon 2022</h5>
             <p>The wait is over, call your friends and join now!</p>
           </div>
         </div>
         <div className="carousel-item">
-          <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp" className="d-block w-100"
-            alt="Cliff Above a Stormy Sea" />
+          <img src="https://www.jobstreet.com.ph/career-resources/wp-content/uploads/sites/6/2021/04/C_EI_PH_A_Working-in-the-Province_-Join-JobStreet%E2%80%99s-Virtual-Career-Fair-scaled.jpg" className="d-block w-100"
+            alt="Cliff Above a Stormy Sea" height='600' />
           <div className="carousel-caption d-none d-md-block">
             <h5>SyraTech Career Fair</h5>
             <p>Advance your career to the next level !</p>
