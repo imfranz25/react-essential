4636fceffc0bb75dd1ea823fe74f6823c80493a6
Francis Ong <francis.ong25@gmail.com>
2022-04-25 22:30:25 +0800

    add payload -> add function 10

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import { legacy_createStore } from "redux";
 const reducerFn = (state = {counter: 0}, action) => {
   if (action.type === 'INC') return {counter: state.counter + 1};
   if (action.type === 'DEC') return {counter: state.counter - 1};
+  if (action.type === 'ADD') return {counter: state.counter + action.payload};
   return state;
 }
 
